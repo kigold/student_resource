e0368d306b6c045a9757d32f8ec704b8d0b5ab72
kingslee <kingsleybox@yahoo.com>
2017-11-13 04:52:45 +0100

    Added delete user function

---
+++ public/javascripts/deleteUser.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    var mBtn = $('#delete_user_btn');
+    var mEmail = $('#emailField')[0].value;
+    //hide or show delete btn
+    if (mEmail != window.localStorage.getItem('email')){
+        $('#delete_user_btn').hide();
+    }
+    mBtn.click(function(){
+        console.log("Deleting user");
+        delUser(mEmail);
+    });
+   
+});
+
+function delUser(email) {
+    console.log('sending data...');;
+    $.ajax({
+        url: "/student/"+email,
+        type: 'DELETE',
+        //contentType: 'json',
+        //processData: false,
+        //data: Qs,
+        success: function(R){
+            console.log(R);
+            window.localStorage.removeItem('name');
+            window.localStorage.removeItem('userid');
+            window.localStorage.removeItem('token');
+            window.localStorage.removeItem('email');
+            window.location = "/";
+        },
+        error:function(x,s,R){
+            console.log(R);
+        },
+    });    
+     
+}
